5a09a4843de12e2e1efec677146bf417a03bc7c6
Piranon Nawakulpun <piranon.n@olx.co.th>
2015-05-30 00:53:02 +0700

    Refactor TweetsController and unit test

--- app/controllers/TweetsController.php
+++ app/controllers/TweetsController.php
@@ -8,9 +8,6 @@ class TweetsController extends \Phalcon\Mvc\Controller {
 	    $options['city'] = $this->request->getPost('city');
 	    $options['lat'] = $this->request->getPost('lat');
 	    $options['lng'] = $this->request->getPost('lng');
-// 	    $options = array(
-// 	        'city' => 'กรุงเทพมหานคร', 'lat' => '13.7563309', 'lng' => '100.50176510000006'
-// 	    );
         $this->tweetsLibrary->setConfig($this->config);
         $this->tweetsLibrary->setTwitterAPIExchange($this->twitterAPIExchange);
         $tweets = $this->tweetsLibrary->getTweets($options);
--- tests/unit/TweetsControllerTest.php
+++ tests/unit/TweetsControllerTest.php
@@ -3,6 +3,7 @@
 class TweetsControllerTest extends BaseUnitTest
 {
     private $tweetsController;
+    private $mockRequest;
     private $mockResponse;
     private $mockTweetsLibrary;
     private $mockTwitterAPIExchange;
@@ -11,9 +12,11 @@ class TweetsControllerTest extends BaseUnitTest
     {
         parent::setUp();
         $this->tweetsController = new TweetsController();
+        $this->mockRequest = $this->getMockBuilder('\Phalcon\Http\Request')->disableOriginalConstructor()->getMock();
         $this->mockResponse = $this->getMockBuilder('\Phalcon\Http\Response')->disableOriginalConstructor()->getMock();
         $this->mockTweetsLibrary = $this->getMockBuilder('TweetsLibrary')->disableOriginalConstructor()->getMock();
         $this->mockTwitterAPIExchange = $this->getMockBuilder('TwitterAPIExchange')->disableOriginalConstructor()->getMock();
+        $this->di->set('request', $this->mockRequest);
         $this->di->set('response', $this->mockResponse);
         $this->di->set('tweetsLibrary', $this->mockTweetsLibrary);
         $this->di->set('twitterAPIExchange', $this->mockTwitterAPIExchange);
@@ -23,8 +26,11 @@ class TweetsControllerTest extends BaseUnitTest
     {
     }
 
-    public function testGetTweetsReturnJsonSuccess()
+    public function testGetTweetFromTwitterAPI()
     {
+        $this->mockRequest->expects($this->at(0))->method('city');
+        $this->mockRequest->expects($this->at(1))->method('lat');
+        $this->mockRequest->expects($this->at(2))->method('lng');
         $this->mockTweetsLibrary->expects($this->once())->method('setConfig');
         $this->mockTweetsLibrary->expects($this->once())->method('setTwitterAPIExchange');
         $this->mockTweetsLibrary->expects($this->once())->method('getTweets');
