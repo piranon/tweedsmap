5626b5bbb1f84fef24c28a9ce6aa92aeb03e5e16
Piranon Nawakulpun <piranon.n@olx.co.th>
2015-05-30 03:08:51 +0700

    Implemented get function in Tweets Model for get tweets cache in mongodb

--- app/models/TweetsModel.php
+++ app/models/TweetsModel.php
@@ -9,6 +9,21 @@ class TweetsModel {
         $this->mongo = $mongo;
     }
 
+    public function get($city)
+    {
+        $tweets = array();
+        $where = array(
+            'city' => (string) $city
+        );
+        $collection = $this->mongo->selectCollection('tweets');
+        $cursor = $collection->find($where);
+        $cacheData = $this->iteratorToArray($cursor);
+        if (!empty($cacheData['tweets'])) {
+            $tweets = $cacheData['tweets'];
+        }
+        return $tweets;
+    }
+
 	public function save($city, $tweets)
 	{
 	    $expireAt = $this->convertIsoDateToMongoDate(date('Y-m-d H:i:s'));
@@ -22,5 +37,11 @@ class TweetsModel {
 	private function convertIsoDateToMongoDate($dateTime)
 	{
 	    return new MongoDate(strtotime($dateTime));
+	}
+
+	public function iteratorToArray($cursor)
+	{
+	    $cursor = iterator_to_array($cursor);
+	    return array_values($cursor);
 	}
 }
--- tests/unit/TweetsModelTest.php
+++ tests/unit/TweetsModelTest.php
@@ -5,13 +5,18 @@ class TweetsModelTest extends BaseUnitTest
     private $tweetsModel;
     private $mockMongo;
     private $mockMongoCollection;
+    private $mockMongoCursor;
 
     protected function setUp()
     {
         parent::setUp();
         $this->mockMongo = $this->getMockBuilder('MongoClient')->disableOriginalConstructor()->getMock();
         $this->mockMongoCollection = $this->getMockBuilder('MongoCollection')->disableOriginalConstructor()->getMock();
-        $this->tweetsModel = new TweetsModel();
+        $this->mockMongoCursor = $this->getMockBuilder('MongoCursor')->disableOriginalConstructor()->getMock();
+        $method = array(
+            'iteratorToArray'
+        );
+        $this->tweetsModel = $this->getMock('TweetsModel', $method);
         $this->tweetsModel->setMongo($this->mockMongo);
     }
 
@@ -19,7 +24,7 @@ class TweetsModelTest extends BaseUnitTest
     {
     }
 
-    public function testInsertTweetsCache()
+    public function testSaveTweetsCache()
     {
         $dateTime = date('Y-m-d H:i:s');
         $tweets = array(
@@ -37,5 +42,29 @@ class TweetsModelTest extends BaseUnitTest
         $this->mockMongo->expects($this->once())->method('selectCollection')->with('tweets')->will($this->returnValue($this->mockMongoCollection));
         $this->mockMongoCollection->expects($this->once())->method('insert')->with($tweetsCache)->will($this->returnValue($this->mockMongoCollection));
         $this->tweetsModel->save('กรุงเทพมหานคร', $tweets);
+    }
+
+    public function testGetTweetsCache()
+    {
+        $expected = array(
+            array(
+                'text' => 'ร้อน (@ กรุงเทพมหานคร (Bangkok) in Bangkok, Thailand) https://t.co/boganjD9cs',
+                'createdAt' => '2015-05-29 12:11:21', 'userName' => 'เสกไม่โอเค',
+                'userProfileImageUrl' => 'http://pbs.twimg.com/profile_images/603604862765387776/cADbF-Ih_normal.jpg',
+                'lat' => 13.75274551, 'lng' => 100.49402475
+            )
+        );
+        $tweetsCache = array(
+            'city' => 'กรุงเทพมหานคร', 'expireAt' => new MongoDate(strtotime('2015-05-30 02:42:00')),
+            'tweets' => $expected
+        );
+        $where = array(
+            'city' => 'กรุงเทพมหานคร'
+        );
+        $this->mockMongo->expects($this->once())->method('selectCollection')->with('tweets')->will($this->returnValue($this->mockMongoCollection));
+        $this->mockMongoCollection->expects($this->once())->method('find')->with($where)->will($this->returnValue($this->mockMongoCursor));
+        $this->tweetsModel->expects($this->once())->method('iteratorToArray')->will($this->returnValue($tweetsCache));
+        $tweets = $this->tweetsModel->get('กรุงเทพมหานคร');
+        $this->assertEquals($expected, $tweets);
     }
 }
